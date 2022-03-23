b766168a390a77c3d33e9e1aca35dad09e816bb4
abdokin <laarichabdo@gmail.com>
2022-03-23 15:05:00 +0100

    gitingnore

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
+#include "data/struct.h"
+#include <stdlib.h>
 int main(){
     printf("im working ...");
+    ptree binary = (ptree)malloc(sizeof(ptree));
+    printf("done ");
     return 0;
 }
