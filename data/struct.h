7670034ba2e2bc27b8ef126501105cde1344d393
abdokin <laarichabdo@gmail.com>
2022-03-23 14:55:56 +0100

    creating binary tree struct

---
+++ data/struct.h
@@ -0,0 +1,18 @@
+
+#ifndef BINARY_TREE
+#define BINARY_TREE
+
+struct tree
+{
+    int v;
+    struct tree * left;
+    struct tree * right;
+
+};
+typedef struct tree * ptree;
+
+#endif // MACRO
+
+
+
+
